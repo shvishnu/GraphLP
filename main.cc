a5792b3fc707a0337eaca437284f2ae5aeb5ed70
Shree Vishnu <shreevishnu2282@gmail.com>
2021-07-26 17:36:03 +0530

    minor changes

--- main.cc
+++ main.cc
@@ -9,7 +9,6 @@ int main(int argc, char* argv[]) {
     int c = 0;
     bool A_proposing = true;
     const char* input_file = nullptr;
-    const char* output_file = nullptr;
     const char* quad_file = nullptr;
     const char* tri_file = nullptr;
 
@@ -21,18 +20,14 @@ int main(int argc, char* argv[]) {
     // -c computes the many-to-one popular matching
     // -i is the path to the input graph, -o is the path where the matching
     // computed should be stored
-    while ((c = getopt(argc, argv, "i:o:q:t:")) != -1) {
+    while ((c = getopt(argc, argv, "i:q:t:")) != -1) {
         switch (c) {
         case 'i': input_file = optarg; break;
-        case 'o': output_file = optarg; break;
         case 'q': quad_file = optarg; break;
         case 't': tri_file = optarg; break;
         case '?':
             if (optopt == 'i') {
                 std::cerr << "Option -i requires an argument.\n";
-            }
-            else if (optopt == 'o') {
-                std::cerr << "Option -o requires an argument.\n";
             }
             else {
                 std::cerr << "Unknown option: " << (char)optopt << '\n';
@@ -40,10 +35,6 @@ int main(int argc, char* argv[]) {
             break;
         default: break;
         }
-    }
-
-    if (not input_file or not output_file) {
-        // do not proceed if file names are not valid
     }
     GraphReader reader1(input_file);
     GraphReader reader2(input_file);
--- solver/reverse.sh
+++
@@ -1,3 +0,0 @@
-source env/bin/activate
-python3 convert.py quadsol.xml>quadtemp.txt
-python3 convert.py trisol.xml>tritemp.txt
